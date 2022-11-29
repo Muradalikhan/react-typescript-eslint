10915fdf5f4300290ad017c908e981d8663aec49
Murad khan <murad.jaddoon@gmail.com>
2022-11-30 01:19:08 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = (): JSX.Element => {
+  return (
+    <div className="App">
+      <h1>Welcome!</h1>
+    </div>
+  );
+};
+
+export default App;
